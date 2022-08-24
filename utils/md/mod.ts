f82f07d937537844e05957e564714caf67b9ffae
Shreyas Mididoddi <shreyassanthu77@gmail.com>
2022-08-24 17:27:08 +0530

    utils/md: added customizeablity to rendering

--- utils/md/mod.ts
+++ utils/md/mod.ts
@@ -9,6 +9,10 @@ import frontMatter from "https://esm.sh/front-matter@4.0.2";
 const codeClasses = "not-prose code-wrapper-22707402-bd3br";
 
 class Renderer extends marked.Renderer {
+  constructor(private showCopyButton: boolean = true) {
+    super();
+  }
+
   heading(
     text: string,
     level: 1 | 2 | 3 | 4 | 5 | 6,
@@ -30,11 +34,15 @@ class Renderer extends marked.Renderer {
       return `<pre><code>${htmlEscape(code)}</code></pre>`;
     }
     const html = Prism.highlight(code, grammar, language!);
-    return `<div class="${codeClasses} highlight highlight-source-${language}"><pre>${html}</pre> <button data-copy-code="${btoa(
-      code
-    )}" title="copy code to clipboard"><svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+    return `<div class="${codeClasses} highlight highlight-source-${language}"><pre>${html}</pre>${
+      this.showCopyButton
+        ? `<button data-copy-code="${btoa(
+            code
+          )}" title="copy code to clipboard"><svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
     <path stroke-linecap="round" stroke-linejoin="round" d="M8 16H6a2 2 0 01-2-2V6a2 2 0 012-2h8a2 2 0 012 2v2m-6 12h8a2 2 0 002-2v-8a2 2 0 00-2-2h-8a2 2 0 00-2 2v8a2 2 0 002 2z" />
-  </svg></button></div>`;
+  </svg></button>`
+        : ""
+    }</div>`;
   }
 
   link(href: string, title: string, text: string) {
@@ -45,32 +53,44 @@ class Renderer extends marked.Renderer {
   }
 }
 
-marked.setOptions({
-  renderer: new Renderer(),
-});
-
 export { css } from "./css.ts";
 export const script = `(()=>{let e=document.querySelectorAll("[data-copy-code]");for(let t of e){let o=atob(t?.dataset?.copyCode);t.addEventListener("click",()=>{navigator.clipboard.writeText(o)})}})();`;
 
 export function renderMd<Attrs = any, Fm extends boolean = true>(
   md: string,
-  parseFrontMatter: Fm = true as any
-): Fm extends true
-  ? {
-      html: string;
-      attributes: Attrs;
-    }
-  : {
-      html: string;
-    } {
-  if (!parseFrontMatter) {
+  {
+    frontMatter: fm = true as any,
+    code: { showCopyButton = true, highlight = true } = {},
+  }: {
+    frontMatter?: Fm;
+    code?: {
+      highlight?: boolean;
+      showCopyButton?: boolean;
+    };
+  } = {}
+): {
+  html: string;
+  // deno-lint-ignore ban-types
+} & (Fm extends true ? { attributes: Attrs } : {}) {
+  let start = "",
+    end = "";
+
+  if (highlight) {
+    marked.setOptions({
+      renderer: new Renderer(showCopyButton),
+    });
+    start = '<div class=".md-e044334dc0">';
+    end = "</div>";
+  }
+
+  if (!fm) {
     return {
-      html: '<div class=".md-e044334dc0">' + marked.parse(md) + "</div>",
+      html: start + marked.parse(md) + end,
     } as any;
   }
   const { body, attributes } = frontMatter<Attrs>(md);
 
-  const html = '<div class=".md-e044334dc0">' + marked.parse(body) + "</div>";
+  const html = start + marked.parse(body) + end;
 
   return { html, attributes } as any;
 }
