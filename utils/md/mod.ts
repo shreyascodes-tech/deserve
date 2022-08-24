59b3cbeeca9814b99f5ee930cd2e682ac07b63e7
Shreyas Mididoddi <shreyassanthu77@gmail.com>
2022-08-24 14:06:27 +0530

    utils/md: fixed lint error

--- utils/md/mod.ts
+++ utils/md/mod.ts
@@ -51,6 +51,7 @@ marked.setOptions({
 export { css } from "./css.ts";
 export const script = `(()=>{addEventListener("DOMContentLoaded",()=>{let e=document.querySelectorAll("[data-copy-code]");for(let t of e){let o=atob(t?.dataset?.copyCode);t.addEventListener("click",()=>{navigator.clipboard.writeText(o)})}});})();`;
 
+// deno-lint-ignore no-explicit-any
 export function renderMd<Attrs = any>(md: string) {
   const { body, attributes } = frontMatter<Attrs>(md);
 
