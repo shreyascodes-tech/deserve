5bdf5d1907308222b4f3982d9a4a0f48d90b1cc1
Shreyas Mididoddi <shreyassanthu77@gmail.com>
2022-08-29 14:50:11 +0530

    utils/jsx: rewritten the jsx util

--- utils/jsx/jsx.tsx
+++ utils/jsx/jsx.tsx
@@ -1,96 +1,137 @@
 // deno-lint-ignore-file ban-types
 /** @jsx h */
-/** @jsxFrag Fragment */
 import { renderToString } from "https://esm.sh/preact-render-to-string@5.2.2?deps=preact@10.10.6";
-import { h, Fragment, VNode, ComponentChildren } from "./preact.ts";
-import { HEAD_CTX } from "./head.tsx";
+import {
+  h,
+  VNode,
+  ComponentChildren,
+  createContext,
+  useContext,
+} from "./preact.ts";
 
-export interface RenderJSXOptions {
-  html?: h.JSX.HTMLAttributes<HTMLHtmlElement>;
-  headAttrs?: h.JSX.HTMLAttributes<HTMLHeadElement>;
-  bodyAttrs?: h.JSX.HTMLAttributes<HTMLBodyElement>;
+type HeadData = {
+  attrs: h.JSX.HTMLAttributes<HTMLHeadElement>;
+  children: ComponentChildren[];
+};
+
+const HEAD_CTX = createContext<HeadData>({ attrs: {}, children: [] });
+export function Head({
+  children,
+  ...props
+}: h.JSX.HTMLAttributes<HTMLHeadElement>) {
+  const head = useContext(HEAD_CTX);
+  head.attrs = { ...head.attrs, ...props };
+  head.children.push(children);
+  return null;
 }
 
-export interface JSXOptions extends RenderJSXOptions {
-  headers?: HeadersInit;
+type BodyData = {
+  attrs: Omit<h.JSX.HTMLAttributes<HTMLBodyElement>, "children">;
+};
+const BODY_CTX = createContext<BodyData>({ attrs: {} });
+export function Body({
+  children,
+  ...props
+}: h.JSX.HTMLAttributes<HTMLBodyElement>) {
+  const body = useContext(BODY_CTX);
+  body.attrs = { ...body.attrs, ...props };
+  return children;
+}
+
+type HtmlData = { attrs: h.JSX.HTMLAttributes<HTMLHtmlElement> };
+const HTML_CTX = createContext<HtmlData>({ attrs: {} });
+export function Html({
+  children,
+  ...props
+}: h.JSX.HTMLAttributes<HTMLHtmlElement>) {
+  const html = useContext(HTML_CTX);
+  html.attrs = { ...html.attrs, ...props };
+  return children;
+}
+
+const RESP_CTX = createContext<{
+  headers: Headers;
   status?: number;
   statusText?: string;
+}>({ headers: new Headers() });
+export function Res(init: ResponseInit) {
+  const res = useContext(RESP_CTX);
+  res.status = init.status;
+  res.statusText = init.statusText;
+  const newHeaders = new Headers(init.headers);
+  newHeaders.forEach((v, k) => res.headers.append(k, v));
+  return null;
 }
 
-function renderJSX(body: VNode<{}>) {
-  const headComponents: ComponentChildren[] = [];
-
-  const bodyStr = renderToString(
-    <HEAD_CTX.Provider value={headComponents}>{body}</HEAD_CTX.Provider>
-  );
-  const headStr = renderToString(<>{headComponents}</>);
-
-  return { head: headStr, body: bodyStr };
-}
-
-function generateHTML(
-  head: string,
-  body: string,
-  options: RenderJSXOptions = {}
-) {
-  const htmlOpts: h.JSX.HTMLAttributes<HTMLHtmlElement> = {
-    lang: "en",
-    ...(options.html ?? {}),
+export interface RenderContext {
+  response: {
+    headers: Headers;
+    status?: number;
+    statusText?: string;
   };
-
-  const headStr =
-    renderToString(
-      <>
-        <meta charSet="UTF-8" />
-        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      </>
-    ) + head;
-
-  const html = renderToString(
-    <html {...htmlOpts}>
-      <head
-        {...(options.headAttrs ?? {})}
-        dangerouslySetInnerHTML={{ __html: headStr }}
-      />
-      <body
-        {...(options.bodyAttrs ?? {})}
-        dangerouslySetInnerHTML={{ __html: body }}
-      />
-    </html>
-  );
-
-  return "<!DOCTYPE html>" + html;
+  body: string;
+  htmlData: HtmlData;
+  head: HeadData;
+  bodyData: BodyData;
 }
 
 export interface CreateJSXOptions {
-  transformBody?(body: VNode<{}>): VNode;
-  transformBodyStr?(body: string): string | Promise<string>;
-  transformHeadStr?(head: string, body: string): string | Promise<string>;
+  transformBody?(body: VNode<{}>): VNode<{}>;
+  transform?(ctx: RenderContext): void | Promise<void>;
 }
 
-export function createJsx(createOptions: CreateJSXOptions = {}) {
-  return async function $jsx(body: VNode<{}>, options: JSXOptions = {}) {
-    body = createOptions.transformBody?.(body) ?? body;
-    let { body: bodyStr, head: headStr } = renderJSX(body);
+export function createJSX(options: CreateJSXOptions = {}) {
+  return async function $jsx(body: VNode<{}>) {
+    const ctx: RenderContext = {
+      body: "",
+      response: {
+        headers: new Headers([["Content-Type", "text/html; charset=utf-8"]]),
+      },
+      bodyData: { attrs: {} },
+      head: {
+        attrs: {},
+        children: [
+          <meta charSet="UTF-8" />,
+          <meta http-equiv="X-UA-Compatible" content="IE=edge" />,
+          <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1.0"
+          />,
+        ],
+      },
+      htmlData: { attrs: {} },
+    };
 
-    bodyStr = (await createOptions.transformBodyStr?.(bodyStr)) ?? bodyStr;
-    headStr =
-      (await createOptions.transformHeadStr?.(headStr, bodyStr)) ?? headStr;
+    ctx.body = renderToString(
+      <RESP_CTX.Provider value={ctx.response}>
+        <HTML_CTX.Provider value={ctx.htmlData}>
+          <HEAD_CTX.Provider value={ctx.head}>
+            <BODY_CTX.Provider value={ctx.bodyData}>
+              {options.transformBody?.(body) ?? body}
+            </BODY_CTX.Provider>
+          </HEAD_CTX.Provider>
+        </HTML_CTX.Provider>
+      </RESP_CTX.Provider>
+    );
 
-    const html = generateHTML(headStr, bodyStr, options);
+    await options.transform?.(ctx);
 
-    const headers = new Headers(options.headers);
-    headers.append("Content-Type", "text/html; charset=utf-8");
+    const html =
+      "<!DOCTYPE html" +
+      renderToString(
+        <html {...ctx.htmlData.attrs}>
+          <head {...ctx.head.attrs}>{ctx.head.children}</head>
+          <body
+            {...ctx.bodyData.attrs}
+            dangerouslySetInnerHTML={{ __html: ctx.body }}
+          />
+        </html>
+      );
 
-    return new Response(html, {
-      headers,
-      status: options.status,
-      statusText: options.statusText,
-    });
+    return new Response(html, ctx.response);
   };
 }
 
-export const jsx = createJsx();
+export const jsx = createJSX();
 
 export { renderToString as renderJSXToString } from "https://esm.sh/preact-render-to-string@5.2.2";
