2a1b9c9d025e6495ec87dbc748705c45003200ce
Shreyas Mididoddi <shreyassanthu77@gmail.com>
2022-08-29 15:47:08 +0530

    docs: added the warning banner

--- www/components/Layout.tsx
+++ www/components/Layout.tsx
@@ -102,6 +102,25 @@ export function Layout({
   return (
     <>
       <Header hasSidebar={!!sideBar} />
+      <div class="bg-[#111827] w-full">
+        <strong class="bg-amber-500/10  flex justify-center items-center gap-x-3 py-4 border-b-1 border-amber-200/75 text-amber-200">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+            class="w-6 h-6"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M12 10.5v3.75m-9.303 3.376C1.83 19.126 2.914 21 4.645 21h14.71c1.73 0 2.813-1.874 1.948-3.374L13.949 4.88c-.866-1.501-3.032-1.501-3.898 0L2.697 17.626zM12 17.25h.007v.008H12v-.008z"
+            />
+          </svg>
+          Deserve is Still in its early stages, expect breaking changes
+        </strong>
+      </div>
       <div class="w-full h-full md:h-full text-white overflow-y-auto bg-[#111827]">
         <div
           class={`container min-h-full ${
