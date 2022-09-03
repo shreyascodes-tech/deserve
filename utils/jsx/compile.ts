2f23a1c9625924b027d1748a3179cff4caf895e6
Shreyas Mididoddi <shreyassanthu77@gmail.com>
2022-09-03 21:28:29 +0530

    utils/jsx: added esbuild on demand compile support

---
+++ utils/jsx/compile.ts
@@ -0,0 +1,17 @@
+import * as esbuild from "https://deno.land/x/esbuild@v0.14.51/mod.js";
+import { denoPlugin } from "https://deno.land/x/esbuild_deno_loader@0.5.2/mod.ts";
+
+export function buildFile(path: string) {
+  return esbuild
+    .build({
+      entryPoints: [path],
+      plugins: [denoPlugin()],
+      bundle: true,
+      minify: true,
+      write: false,
+      format: "iife",
+    })
+    .then((res) => res.outputFiles[0].text);
+}
+
+export * from "https://deno.land/x/esbuild@v0.14.51/mod.js";
