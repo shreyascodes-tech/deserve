6749b7e0c5b9cc2198035e8c13caf5009c73a504
Shreyas Mididoddi <shreyassanthu77@gmail.com>
2022-09-03 22:04:05 +0530

    utils: support deno deploy*

--- utils/jsx/compile.ts
+++ utils/jsx/compile.ts
@@ -1,6 +1,12 @@
 import * as esbuild from "https://deno.land/x/esbuild@v0.14.51/wasm.js";
 import { denoPlugin } from "https://deno.land/x/esbuild_deno_loader@0.5.2/mod.ts";
 
+if (!window.Worker) {
+  esbuild.initialize({
+    worker: false,
+  });
+}
+
 export function buildFile(path: string) {
   return esbuild
     .build({
