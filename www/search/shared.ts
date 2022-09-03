f911e1fc88db4f9a397a91e22694aee09c7c7b7b
Shreyas Mididoddi <shreyassanthu77@gmail.com>
2022-09-03 21:29:07 +0530

    www: added search support

---
+++ www/search/shared.ts
@@ -0,0 +1,12 @@
+import Fuse from "https://esm.sh/fuse.js@6.6.2";
+
+export const options: Fuse.IFuseOptions<{}> = {
+  includeScore: true,
+  isCaseSensitive: false,
+  ignoreLocation: true,
+  keys: [
+    { name: "attributes.title", weight: 0.6 },
+    { name: "body", weight: 0.4 },
+    { name: "path", weight: 0.2 },
+  ],
+};
